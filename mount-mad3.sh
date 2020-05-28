1c4be7d04af1da6b162b1c85b4c103cd9b20791e
Erik Reinertsen <ereinertsen@mgh.harvard.edu>
2020-05-28 10:14:31 -0400

    mount script has uid and gid permissions

--- mount-mad3.sh
+++ mount-mad3.sh
@@ -6,11 +6,11 @@ SERVER=//mad3.partners.org
 # DESTINATION="/media/lm4-ecg"
 
 # Mount directory on LandMark4 containing CDAC files
-#LABDIR="mgh-neuro-cdac/projects/"
-#DESTINATION="/media/mad3"
+LABDIR="mgh-neuro-cdac/projects/"
+DESTINATION="/media/mad3"
 
 # Mount directory on LandMark4 containing Bedmaster data
-LABDIR="PHS-RISC-LM4/ConvertedData/bedmaster"
-DESTINATION="/media/lm4-bedmaster/"
+#LABDIR="PHS-RISC-LM4/ConvertedData/bedmaster"
+#DESTINATION="/media/lm4-bedmaster/"
 
-sudo mount -t cifs -o user=$USER,uid=1000 $SERVER/$LABDIR $DESTINATION
+sudo mount -t cifs -o user=$USER,uid=$(id -u),gid=$(id -g) $SERVER/$LABDIR $DESTINATION
