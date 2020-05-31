16f20eed556f6fb05d2ce2a0367153a0799bed73
Erik Reinertsen <ereinertsen@mgh.harvard.edu>
2020-05-31 16:19:30 -0400

    clean mount scripts

--- mount-mad3.sh
+++ mount-mad3.sh
@@ -3,14 +3,14 @@ SERVER=//mad3.partners.org
 
 # Mount directory on LandMark4 containing daily ECG dumps
 # LABDIR="PHS-RISC-LM4/MUSE_DATA"
-# DESTINATION="/media/lm4-ecg"
+# MOUNTPOINT="/media/lm4-ecg"
 
 # Mount directory on LandMark4 containing CDAC files
 LABDIR="mgh-neuro-cdac/projects/"
-DESTINATION="/media/mad3"
+MOUNTPOINT="/media/mad3"
 
 # Mount directory on LandMark4 containing Bedmaster data
 #LABDIR="PHS-RISC-LM4/ConvertedData/bedmaster"
-#DESTINATION="/media/lm4-bedmaster/"
+#MOUNTPOINT="/media/lm4-bedmaster/"
 
-sudo mount -t cifs -o user=$USER,uid=$(id -u),gid=$(id -g) $SERVER/$LABDIR $DESTINATION
+sudo mount -t cifs -o user=$USER,uid=$(id -u),gid=$(id -g) $SERVER/$LABDIR $MOUNTPOINT
