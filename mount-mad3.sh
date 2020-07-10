c1bac117bc9ff16e4a0222ee24e37cddeef3d2d9
aguirrelab <aguirrelab@montserrat.partners.org>
2020-07-10 16:03:18 -0400

    adds domain to login script

--- mount-mad3.sh
+++ mount-mad3.sh
@@ -2,15 +2,16 @@ USER=ereinertsen@mgh.harvard.edu
 SERVER=//mad3.partners.org
 
 # Mount directory on LandMark4 containing daily ECG dumps
-LABDIR="PHS-RISC-LM4/MUSE_DATA"
-MOUNTPOINT="/media/lm4-ecg"
+#LABDIR="PHS-RISC-LM4/MUSE_DATA"
+#MOUNTPOINT="/media/lm4-ecg"
 
 # Mount directory on LandMark4 containing CDAC files
 #LABDIR="mgh-neuro-cdac/projects/"
 #MOUNTPOINT="/media/mad3"
 
 # Mount directory on LandMark4 containing Bedmaster data
-#LABDIR="PHS-RISC-LM4/ConvertedData/bedmaster"
-#MOUNTPOINT="/media/lm4-bedmaster/"
+LABDIR="PHS-RISC-LM4/ConvertedData/bedmaster"
+#LABDIR="PHS-RISC-LM4"
+MOUNTPOINT="/media/lm4-bedmaster"
 
-sudo mount -t cifs -o user=$USER,uid=$(id -u),gid=$(id -g aguirrelab) $SERVER/$LABDIR $MOUNTPOINT
+sudo mount -t cifs -o user=$USER,uid=$(id -u),gid=$(id -g aguirrelab),domain=partners $SERVER/$LABDIR $MOUNTPOINT
