0c18a846ae5cf61077f8d5f4a4b65ff8de56ebe6
Erik Reinertsen <ereinertsen@mgh.harvard.edu>
2020-08-03 12:23:41 -0400

    mad3 cdac

--- mount-mad3.sh
+++ mount-mad3.sh
@@ -1,18 +1,18 @@
 USER=er498
-#SERVER=//mad3.partners.org
-SERVER=//ideafs.partners.org
+SERVER=//mad3.partners.org
+#SERVER=//ideafs.partners.org
 
-# Mount directory on LandMark4 containing daily ECG dumps
+# Mount LandMark4 daily ECG dumps
 #LABDIR="PHS-RISC-LM4/MUSE_DATA"
 #MOUNTPOINT="/media/lm4-ecg"
 
-# Mount directory on LandMark4 containing CDAC files
-#LABDIR="mgh-neuro-cdac/projects/"
-#MOUNTPOINT="/media/mad3"
+# Mount MAD3 CDAC server
+LABDIR="mgh-neuro-cdac/projects/"
+MOUNTPOINT="/media/cdac"
 
-# Mount directory on LandMark4 containing Bedmaster data
-LABDIR="PHS-RISC-LM4/ConvertedData/bedmaster"
+# Mount LandMark4 Bedmaster
+#LABDIR="PHS-RISC-LM4/ConvertedData/bedmaster"
 #LABDIR="PHS-RISC-LM4"
-MOUNTPOINT="/media/lm4-bedmaster"
+#MOUNTPOINT="/media/lm4-bedmaster"
 
 sudo mount -t cifs -o user=$USER,uid=$(id -u),gid=$(id -g aguirrelab),domain=partners $SERVER/$LABDIR $MOUNTPOINT
