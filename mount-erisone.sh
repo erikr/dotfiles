3725090bbb76ea9ad73c1fc530d55f77ef8bbdbc
Erik Reinertsen <ereinertsen@mgh.harvard.edu>
2020-08-20 21:36:31 -0400

    revise mount script

--- mount-erisone.sh
+++
@@ -1,8 +0,0 @@
-USER=er498
-SERVER=//eris1fs2.partners.org
-MOUNTPOINT=/media/erisone_er498
-
-sudo mount -t cifs -o user=$USER,uid=$(id -u),gid=$(id -g),vers=1.0 $SERVER/$USER $MOUNTPOINT
-
-# sudo sshfs -o allow_other $USER@erisone.partners.org:/PHShome/$USER $MOUNTDIR
-# sudo mount -t cifs -o username=$USER,domain=partners,vers=1.0,uid=1000 //eris1fs2.partners.org/$USER $MOUNTDIR
--- mount.sh
+++ mount.sh
@@ -6,9 +6,9 @@ SERVER=//mad3.partners.org
 #LABDIR="PHS-RISC-LM4/MUSE_DATA"
 #MOUNTPOINT="/media/lm4-ecg"
 
-# Mount MAD3 CDAC server
+# Mount MAD3
 LABDIR="mgh-neuro-cdac/projects/ml4icu"
-MOUNTPOINT="/media/cdac"
+MOUNTPOINT="/media/mad3"
 
 # Mount LandMark4 Bedmaster
 #LABDIR="PHS-RISC-LM4/ConvertedData/bedmaster"
@@ -16,3 +16,8 @@ MOUNTPOINT="/media/cdac"
 #MOUNTPOINT="/media/lm4-bedmaster"
 
 sudo mount -t cifs -o user=$LOGIN,uid=$(id -u),gid=$(id -g),domain=partners $SERVER/$LABDIR $MOUNTPOINT
+
+# Mount ERISOne
+#SERVER=//eris1fs2.partners.org
+#MOUNTPOINT=/media/erisone
+#sudo mount -t cifs -o user=$LOGIN,uid=$(id -u),gid=$(id -g),vers=1.0 $SERVER/$USER $MOUNTPOINT
