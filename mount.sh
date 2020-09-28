cfc870592dd7af3af351f444386fbe02f173eccd
Erik Reinertsen <ereinertsen@mgh.harvard.edu>
2020-09-28 09:16:31 -0400

    mount script takes arg

--- mount.sh
+++ mount.sh
@@ -1,23 +1,23 @@
 LOGIN=er498
 SERVER=//mad3.partners.org
-#SERVER=//ideafs.partners.org
 
-# Mount LandMark4 daily ECG dumps
-#LABDIR="PHS-RISC-LM4/MUSE_DATA"
-#MOUNTPOINT="/media/lm4-ecg"
-
-# Mount MAD3
-LABDIR="mgh-neuro-cdac/projects"
-MOUNTPOINT="/media/mad3"
-
-# Mount LandMark4 Bedmaster
-#LABDIR="PHS-RISC-LM4/ConvertedData/bedmaster"
-#LABDIR="PHS-RISC-LM4"
-#MOUNTPOINT="/media/lm4-bedmaster"
-
-sudo mount -t cifs -o user=$LOGIN,uid=$(id -u),gid=$(id -g),domain=partners $SERVER/$LABDIR $MOUNTPOINT
+# Set server within MAD3 and mount point on local machine
+if [[ "$1" == "ml4icu" ]]; then
+    LABDIR="mgh-neuro-cdac/projects/ml4icu"
+    MOUNTPOINT="/media/mad3"
+elif [[ "$1" == "lm4" ]]; then
+    LABDIR="PHS-RISC-LM4/ConvertedData/bedmaster"
+    MOUNTPOINT="/media/lm4-bedmaster"
+elif [[ "$1" == "ecg" ]]; then
+    LABDIR="PHS-RISC-LM4/MUSE_DATA"
+    MOUNTPOINT="/media/lm4-ecg"
+fi
 
 # Mount ERISOne
-#SERVER=//eris1fs2.partners.org
-#MOUNTPOINT=/media/erisone
-#sudo mount -t cifs -o user=$LOGIN,uid=$(id -u),gid=$(id -g),vers=1.0 $SERVER/$USER $MOUNTPOINT
+if [[ "$1" == "erisone" ]]; then
+    SERVER=//eris1fs2.partners.org
+    MOUNTPOINT=/media/erisone
+    sudo mount -t cifs -o user=$LOGIN,uid=$(id -u),gid=$(id -g),vers=1.0 $SERVER/$USER $MOUNTPOINT
+else
+    sudo mount -t cifs -o user=$LOGIN,uid=$(id -u),gid=$(id -g),domain=partners $SERVER/$LABDIR $MOUNTPOINT
+fi
