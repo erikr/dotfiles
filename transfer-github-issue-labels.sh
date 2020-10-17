a09ca8be349d9cd78cc25fcb99c73aa5577078a3
Erik Reinertsen <ereinertsen@mgh.harvard.edu>
2020-10-17 09:44:47 -0400

    xfer github issue now takes owner repo_name as args

--- transfer-github-issue-labels.sh
+++ transfer-github-issue-labels.sh
@@ -7,15 +7,16 @@
 # access the source and target repositories.
 # This is how you authorize with the GitHub API.
 # https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line
-GH_TOKEN=
+GH_TOKEN=b38d745c24dad9111ac62d2e1ba2b6a3db3207d8
+
 
 # The source repository whose labels to copy.
 SRC_GH_USER=aguirre-lab
-SRC_GH_REPO=ml
+SRC_GH_REPO=ml4c3
 
 # The target repository to add or update labels.
-TGT_GH_USER=mit-ccrg
-TGT_GH_REPO=covid
+TGT_GH_USER=$1
+TGT_GH_REPO=$2
 
 # ---------------------------------------------------------
 
