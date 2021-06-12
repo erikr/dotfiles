3ac5b3948ea66d583eba4012fccc67a9f52a041a
Erik Reinertsen <erikrtn@gmail.com>
2021-06-12 09:25:56 -0400

    Switch to neovim

--- symlink.sh
+++ symlink.sh
@@ -5,7 +5,16 @@ for file in $files; do
     rm $HOME/$file
         
     if [[ "$1" == "make" ]]; then
-        echo "Making symlinks from $HOME/$file to $DIR/$file"
+        echo "Symlinking $HOME/$file to $DIR/$file"
         ln -s $DIR/$file $HOME/$file
     fi
 done
+
+# Neovim
+echo "Attempting to delete ~/.config/nvim/init.vim"
+rm ~/.config/nvim/init.vim
+if [[ "$1" == "make" ]]; then
+    echo "Symlinking $DIR/init.vim to ~/.config/nvim/init.vim"
+    ln -s $DIR/init.vim ~/.config/nvim/init.vim
+fi
+
